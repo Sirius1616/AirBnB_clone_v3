f824fbb401c00148112580494758e46d76d7b803
John Ezekiel <siriusa1.615@gmail.com>
2024-08-07 00:30:32 +0100

    RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ This module contains endpoint(route) status
 from flask import Flask
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -13,3 +14,14 @@ def status():
     Returns a JSON status
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Retrieves the number of each object by type"""
+    return jsonify({"amenities": storage.count("Amenity"), 
+                    "cities": storage.count("City"), 
+                    "places": storage.count("Place"), 
+                    "reviews": storage.count("Review"), 
+                    "states": storage.count("State"), 
+                    "users": storage.count("User")})
+
